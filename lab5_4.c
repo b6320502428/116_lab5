e359c25938fe57beaf7aa4d0ffad64c71674c5af
Bowornwat Thongoou <Bowornwat.b@ku.th>
2021-03-08 19:55:59 +0700

    lab5_4 up5

--- lab5_4.c
+++ lab5_4.c
@@ -3,34 +3,15 @@
 #include <string.h>
 int main ()
 {
-    int i,j,dn,pn,an;
+    int i,j,dn,pn,an,ans=0;
     char d[16],p[16],a[16];
     scanf("%s %s %s",&d,&p,&a);
-    /*for(i=0; i<strlen(d); i++)
-    {
-        if(p[i]>47&&p[i]<58)
-            z++;
-    }
-    for(i=0; i<strlen(p); i++)
-    {
-        if(p[i]>47&&p[i]<58)
-            z++;
-    }
-    for(i=0; i<strlen(a); i++)
-    {
-        if(p[i]>47&&p[i]<58)
-            z++;
-    }
-    if(z==(strlen(d)+strlen(p)+strlen(a)))
-    {
-
-    }*/
     for(i=2; i<37; i++)
     {
         dn=0;
         pn=0;
         an=0;
-        for(j=strlen(d)-1; j>=0; j--)
+        for(j=0; j<strlen(d); j++)
         {
             if(d[j]>47&&d[i]<58)
             {
@@ -41,7 +22,7 @@ int main ()
                 dn=(dn*i)+(d[j]-55);
             }
         }
-        for(j=strlen(p)-1; j>=0; j--)
+        for(j=0; j<strlen(p); j++)
         {
             if(p[j]>47&&p[i]<58)
             {
@@ -52,7 +33,7 @@ int main ()
                 pn=(pn*i)+(p[j]-55);
             }
         }
-        for(j=strlen(a)-1; j>=0; j--)
+        for(j=0; j<strlen(a); j++)
         {
             if(a[j]>47&&a[i]<58)
             {
@@ -63,6 +44,15 @@ int main ()
                 an=(an*i)+(a[j]-55);
             }
         }
+        if(an==(dn+pn))
+        {
+            ans=i;
+            break;
+        }
     }
+    if(ans!=0)
+        printf("%d",ans);
+    else
+        printf("no");
     return 0;
 }
