8a15761e340dfd34e5a80f6691d5eee1eaccbaca
Bowornwat Thongoou <Bowornwat.b@ku.th>
2021-03-08 18:14:27 +0700

    lab5_5 up2

--- lab5_5.c
+++ lab5_5.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main()
 {
-    int i,j,k,l,n,m;
+    int i,j,k,l,n,m,max=-32768,tmp;
     scanf("%d",&n);
     scanf("%d",&m);
     int a[n][n],b[m][m];
@@ -16,12 +16,18 @@ int main()
     {
         for(l=0; l<n-(m-1); l++)
         {
+            tmp=0;
             for(i=0; i<m; i++)
             {
                 for(j=0; j<m; j++)
                 {
                     b[i][j]=a[k+i][l+j];
+                    tmp+=b[i][j];
                 }
+            }
+            if(tmp>max)
+            {
+                max=tmp;
             }
         }
     }
