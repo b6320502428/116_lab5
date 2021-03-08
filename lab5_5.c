9f9a9a8895459db37ade04301bc3b57e278f23f7
Bowornwat Thongoou <Bowornwat.b@ku.th>
2021-03-08 18:08:59 +0700

    lab5_5 up1

--- lab5_5.c
+++ lab5_5.c
@@ -1,10 +1,29 @@
 #include <stdio.h>
 int main()
 {
-    int i,j,k,n,m;
+    int i,j,k,l,n,m;
     scanf("%d",&n);
     scanf("%d",&m);
     int a[n][n],b[m][m];
-
+    for(i=0; i<n; i++)
+    {
+        for(j=0; j<n; j++)
+        {
+            scanf("%d",&a[i][j]);
+        }
+    }
+    for(k=0; k<n-(m-1); k++)
+    {
+        for(l=0; l<n-(m-1); l++)
+        {
+            for(i=0; i<m; i++)
+            {
+                for(j=0; j<m; j++)
+                {
+                    b[i][j]=a[k+i][l+j];
+                }
+            }
+        }
+    }
     return 0;
 }
