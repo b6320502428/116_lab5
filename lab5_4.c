3062197b6fb01c994fd6598a0e407dba47ba72ab
Bowornwat Thongoou <Bowornwat.b@ku.th>
2021-03-08 19:18:10 +0700

    lab5_4 up1

--- lab5_4.c
+++ lab5_4.c
@@ -1,7 +1,25 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 int main ()
 {
-    int d,p,a;
-    scanf("%d %d %d",&d,&p,&a);
+    int i,j=0;
+    char d[6],p[6],a[6];
+    scanf("%s %s %s",&d,&p,&a);
+    for(i=0; i<strlen(d); i++)
+    {
+        if(p[i]>48&&p[i]<58)
+            j++;
+    }
+    for(i=0; i<strlen(p); i++)
+    {
+        if(p[i]>48&&p[i]<58)
+            j++;
+    }
+    for(i=0; i<strlen(a); i++)
+    {
+        if(p[i]>48&&p[i]<58)
+            j++;
+    }
     return 0;
 }
