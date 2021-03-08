b1929cbe390488859f8cf7b3f10bf5c979ac6fc4
Bowornwat Thongoou <Bowornwat.b@ku.th>
2021-03-08 16:59:37 +0700

    lab5_2 up2

--- lab5_2.c
+++ lab5_2.c
@@ -5,7 +5,25 @@ int main()
     scanf("%d",&n);
     if(n%2==0)
     {
+        for(i=0; i<=n/2; i++)
+        {
+            for(j=0; j<n/2-i-1; j++)
+            {
+                printf("_");
+            }
+            printf("*");
+            if(i==0)
+            {
+                for(j=0; j<n/2-i-1; j++)
+                {
+                    printf("_");
+                }
+            }
+        }
+        for(i=0; i<n/2; i++)
+        {
 
+        }
     }
     return 0;
 }
