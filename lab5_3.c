2b829778c00d1d2a78281b7101b43fca2a736f49
Bowornwat Thongoou <Bowornwat.b@ku.th>
2021-03-08 17:45:05 +0700

    lab5_3 up1

--- lab5_3.c
+++ lab5_3.c
@@ -1,7 +1,37 @@
 #include <stdio.h>
 int main()
 {
-    int un[3]={2,3,5},i,n;
+    int i,n,j=0,tmp;
     scanf("%d",&n);
+    for(i=2;1;i++)
+    {
+        tmp=i;
+        while(tmp!=1)
+        {
+            if(tmp%2==0)
+            {
+                tmp/=2;
+            }
+            else if(tmp%3==0)
+            {
+                tmp/=3;
+            }
+            else if(tmp%5==0)
+            {
+                tmp/=5;
+            }
+            else
+            {
+                j--;
+                break;
+            }
+        }
+        j++;
+        if(j==n)
+        {
+            break;
+        }
+    }
+    printf("%d",i);
     return 0;
 }
